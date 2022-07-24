91968ffb79861a74466f28c576a688086b992ad2
kyiroywops <aleexisazul@gmail.com>
2022-07-24 19:29:58 -0400

    avance cositos

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const AddNote = () => {
+    const [noteText, setNoteText] = useState('');
+
+    const handleChange = (event) => {
+        console.log(event.target.value);
+    }
     return(
         <div className='note new'>
-            <textarea rows="8" cols="10" placeholder="Ingresa tu nota aquí..."></textarea>
+            <textarea 
+                rows="8" 
+                cols="10" 
+                placeholder="Ingresa tu nota aquí..."
+                value={noteText}
+                onChange={handleChange}
+            ></textarea>
             <div className='note-footer'>
                 <small>200 Remaining</small>
                 <button className='save'>Guardar</button>
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -3,7 +3,7 @@ import AddNote from './AddNote';
 
 const NoteList = ({ notes }) => {
     return(
-        <div className="notes-list">
+        <div className='notes-list'>
             {notes.map((note) => (
                 <Note 
                     id={note.id}
