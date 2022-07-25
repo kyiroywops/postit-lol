124674015304753516861749c5b7dbe4ab675eec
kyiroywops <aleexisazul@gmail.com>
2022-07-24 22:08:45 -0400

    Ultimos cambios

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,9 +2,11 @@ import { MdDeleteForever } from 'react-icons/md';
 
 const Note = ({ id, text, date, handleDeleteNote }) => {
     return (<div className="note">
+        <span>Titulo</span>
         <span>{text}</span>
         <div className="note-footer">
             <small>{date}</small>
+            <small>Importante</small>
             <MdDeleteForever onClick={() => handleDeleteNote(id)} className='delete-icon' size='1.3em' />
         </div>
     </div>
